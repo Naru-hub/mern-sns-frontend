79ed7d1b60df060bd03f8544f6d3fda5ad4342a8
Naru-hub <1208ny0324@gmail.com>
2022-07-11 22:22:42 +0900

    TimeLineコンポーネントの雛形を作成

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./TimeLine.css";
 
 export default function TimeLine() {
-  return <div>TimeLine</div>;
+  return (
+    <div className="timeline">
+      <div className="timelineWrapper">
+        {/* <Share /> */}
+        {/* <Post /> */}
+      </div>
+    </div>
+  );
 }
