556ab5d1645da1a60abd79a932af84038adbf7b1
Naru-hub <1208ny0324@gmail.com>
2022-07-20 20:25:23 +0900

    プロフィールのタイムラインを修正

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -5,14 +5,14 @@ import "./TimeLine.css";
 // import { Posts } from "../../dummyData";
 import axios from "axios";
 
-export default function TimeLine() {
+export default function TimeLine({ username }) {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await axios.get(
-        "/posts/timeline/62c9124ea8086fd4afc248f0"
-      );
+      const response = username
+        ? await axios.get(`/posts/profile/${username}`)
+        : await axios.get("/posts/timeline/62c9124ea8086fd4afc248f0");
       // console.log(response);
       setPosts(response.data);
     };
