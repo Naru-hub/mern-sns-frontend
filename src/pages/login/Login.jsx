830ed80ee2dd5b3262e3eeec49c6a5c6b9b9588d
Naru-hub <1208ny0324@gmail.com>
2022-07-25 20:34:44 +0900

    Emailとパスワードの値をuseRefのHooksを使って取得する

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Login.css";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -10,13 +19,14 @@ export default function Login() {
           <span className="loginDesc">本格的なSNSを自分の手で。</span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
+          <form className="loginBox" onSubmit={(e) => handleSubmit(e)}>
             <p className="loginMsg">ログインはこちら</p>
             <input
               type="email"
               className="loginInput"
               placeholder="Eメール"
               required
+              ref={email}
             />
             <input
               type="password"
@@ -24,11 +34,12 @@ export default function Login() {
               placeholder="パスワード"
               required
               minLength="6"
+              ref={password}
             />
             <button className="loginButton">ログイン</button>
             <span className="loginForgot">パスワードを忘れた方へ</span>
             <button className="loginRegisterButton">アカウント作成</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
