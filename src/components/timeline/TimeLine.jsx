848f4be895b153b1ac3b60ec10e88e932f321d2b
Naru-hub <1208ny0324@gmail.com>
2022-07-27 21:56:16 +0900

    ログイン中のユーザーのタイムラインを表示させる

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./TimeLine.css";
 // import { Posts } from "../../dummyData";
 import axios from "axios";
+import { AuthContext } from "../../state/AuthContext";
 
 export default function TimeLine({ username }) {
   const [posts, setPosts] = useState([]);
 
+  const { user } = useContext(AuthContext);
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = username
-        ? await axios.get(`/posts/profile/${username}`)
-        : await axios.get("/posts/timeline/62c9124ea8086fd4afc248f0");
+        ? await axios.get(`/posts/profile/${username}`) // プロフィールの場合
+        : await axios.get(`/posts/timeline/${user._id}`); // ホームの場合
       // console.log(response);
       setPosts(response.data);
     };
     fetchPosts();
-  }, [username]);
+  }, [username, user._id]);
 
   return (
     <div className="timeline">
