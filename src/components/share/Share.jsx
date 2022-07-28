87211e0539b77e841690e5d6201462185ea8cd12
Naru-hub <1208ny0324@gmail.com>
2022-07-28 21:41:18 +0900

    投稿用のAPIを叩いてタイムラインに投稿する

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,11 +1,29 @@
 import { Analytics, Face, Gif, Image } from "@mui/icons-material";
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { AuthContext } from "../../state/AuthContext";
 import "./Share.css";
+import axios from "axios";
 
 export default function Share() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useContext(AuthContext);
+  const desc = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    try {
+      await axios.post("/posts", newPost);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="share">
@@ -24,11 +42,12 @@ export default function Share() {
             type="text"
             className="shareInput"
             placeholder="今何してるの？"
+            ref={desc}
           />
         </div>
         <hr className="shareHr" />
 
-        <div className="shareButtons">
+        <form className="shareButtons" onSubmit={(e) => handleSubmit(e)}>
           <div className="shareOptions">
             <div className="shareOption">
               <Image className="shareIcon" htmlColor="blue" />
@@ -47,8 +66,10 @@ export default function Share() {
               <span className="shareOptionText">投票</span>
             </div>
           </div>
-          <button className="shareButton">投稿</button>
-        </div>
+          <button className="shareButton" type="submit">
+            投稿
+          </button>
+        </form>
       </div>
     </div>
   );
