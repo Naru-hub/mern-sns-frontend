8e2720699997be7c7a4b182ac642dda644ec69ab
Naru-hub <1208ny0324@gmail.com>
2022-07-30 15:53:28 +0900

    画像アップロード機能実装の前の下準備

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,5 @@
 import { Analytics, Face, Gif, Image } from "@mui/icons-material";
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { AuthContext } from "../../state/AuthContext";
 import "./Share.css";
 import axios from "axios";
@@ -8,6 +8,9 @@ export default function Share() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useContext(AuthContext);
   const desc = useRef();
+
+  const [file, setFile] = useState(null);
+  console.log(file);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -49,10 +52,17 @@ export default function Share() {
 
         <form className="shareButtons" onSubmit={(e) => handleSubmit(e)}>
           <div className="shareOptions">
-            <div className="shareOption">
+            <label className="shareOption" htmlFor="file">
               <Image className="shareIcon" htmlColor="blue" />
               <span className="shareOptionText">写真</span>
-            </div>
+              <input
+                type="file"
+                id="file"
+                accept=".png, .jpeg, .jpg"
+                style={{ display: "none" }}
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
             <div className="shareOption">
               <Gif className="shareIcon" htmlColor="hotpink" />
               <span className="shareOptionText">GIF</span>
